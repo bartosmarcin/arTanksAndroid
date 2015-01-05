fd3e081da11baac83782124cb4a7e0219846a09c
bartosmarcin <bartosmarcin@gmail.com>
2015-01-05 22:28:24 +0100

    refactoring

--- src/com/mygdx/game/android/MarkerDetector.java
+++ src/com/mygdx/game/android/MarkerDetector.java
@@ -15,29 +15,42 @@ import org.opencv.imgproc.Imgproc;
 import android.os.Environment;
 
 public class MarkerDetector {
-	MatOfPoint2f tmpContour = new MatOfPoint2f();
-	MatOfPoint2f outerCurve = new MatOfPoint2f();
-	MatOfPoint2f innerCurve = new MatOfPoint2f();
-	Mat hierarchy = new Mat();
-	
-	private Mat gray = new Mat();
-	private Mat thrs = new Mat();
+	MatOfPoint2f tmpContour;
+	MatOfPoint2f outerCurve;
+	MatOfPoint2f innerCurve;
+	Mat hierarchy;
+	double scaleDown;
+	private Mat thrs;
 	
 
 	private List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 	
-	public List<Marker> detect(Mat rgba) { 
-		Imgproc.cvtColor(rgba, gray, Imgproc.COLOR_RGB2GRAY);
-		Imgproc.blur(gray, gray, new Size(3, 3));
-
-		Imgproc.threshold(gray, thrs, 0, 255, Imgproc.THRESH_OTSU);
+	public MarkerDetector(){
+		this(1);
+	}
 	
-		gray.release();
-		// Imgproc.Canny(gray, thrs, 150, 400, 3, true);
-		Imgproc.findContours(thrs, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+	public MarkerDetector(double scaleDown){
+		tmpContour = new MatOfPoint2f();
+		outerCurve = new MatOfPoint2f();
+		innerCurve = new MatOfPoint2f();
+		thrs = new Mat();
+		this.hierarchy = new Mat();
+		this.scaleDown = scaleDown;
+	}
+	
+	public Marker detect(Mat rgba) { 
+		Marker marker = null;
+		if(scaleDown != 1)
+			Imgproc.resize(thrs, thrs, );
+		Imgproc.cvtColor(rgba, thrs, Imgproc.COLOR_RGB2GRAY);
+//		Imgproc.blur(thrs, thrs, new Size(17,17));
+//		Imgproc.medianBlur(thrs, thrs, 17);
+		Imgproc.threshold(thrs, thrs, 50, 255, Imgproc.THRESH_BINARY);
+//		Imgproc.adaptiveThreshold(thrs, thrs,255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 11, 2);
+//		Imgproc.Canny(thrs, thrs, 150, 400, 3, true);
+		Imgproc.findContours(thrs, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_NONE);
 		
-		thrs.release();
-		List<Marker> detected = new ArrayList<Marker>();
+//		thrs.release();
 
 		int child = -1;
 		for (int i = 0; i < contours.size(); i++) {
@@ -50,23 +63,20 @@ public class MarkerDetector {
 //			
 			contours.get(i).convertTo(tmpContour, CvType.CV_32FC2);
 			Imgproc.approxPolyDP(tmpContour, outerCurve, 0.1 * tmpContour.height(), true);
-			//Log.i("OUTER", "" + outerCurve.height());
 
 			if (outerCurve.height() != 4)
 				continue;
 
 			contours.get(child).convertTo(tmpContour, CvType.CV_32FC2);
 			Imgproc.approxPolyDP(tmpContour, innerCurve, 0.1 * tmpContour.height(), true);
-			//Log.i("INNER", "" + innerCurve.height());
 			if (innerCurve.height() != 6)
 				continue;
-			detected.add(new Marker(innerCurve.toList(), outerCurve.toList()));
-	//		break;
-	
+			marker = new Marker(innerCurve.toList(), outerCurve.toList());
+			break;
 		}
-		for(MatOfPoint p : contours)
-			p.release();
+//		for(MatOfPoint p : contours)
+//			p.release();
 		contours.clear();
-		return detected;
+		return marker;
 	}
 }
